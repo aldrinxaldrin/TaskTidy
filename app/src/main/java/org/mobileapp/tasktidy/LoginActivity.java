b8e8e1833c189472f04021e2b9f96b67a30ca097
Labyrinth-759 <valenzuelajoshua759@gmail.com>
2024-12-01 15:04:08 +0800

    joshua login

---
+++ app/src/main/java/org/mobileapp/tasktidy/LoginActivity.java
@@ -0,0 +1,16 @@
+package org.mobileapp.tasktidy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.mobileapp.tasktidy.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
