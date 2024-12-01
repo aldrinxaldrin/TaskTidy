b80223d584db44430b145fbe65a1da2d43998641
Labyrinth-759 <valenzuelajoshua759@gmail.com>
2024-12-01 14:34:47 +0800

    joshua comment

--- app/src/main/java/org/mobileapp/tasktidy/MainActivity.java
+++ app/src/main/java/org/mobileapp/tasktidy/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.tasktidy.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //joshua
 }
